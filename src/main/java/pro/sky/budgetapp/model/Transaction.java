0814ea6c10d805e0d5630f71e47fa030e8de2ed3
Максим <gmax7911@gmail.com>
2023-02-21 11:13:56 +0300

    1

---
+++ src/main/java/pro/sky/budgetapp/model/Transaction.java
@@ -0,0 +1,15 @@
+package pro.sky.budgetapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
+public class Transaction {
+    private Category category;
+    private int sum;
+    private String comment;
+
+
+}
--- src/main/java/pro/sky/budgetapp/service/impl/BudgetServiceImpl.java
+++ src/main/java/pro/sky/budgetapp/service/impl/BudgetServiceImpl.java
@@ -1,24 +1,56 @@
 package pro.sky.budgetapp.service.impl;
 
 import org.springframework.stereotype.Service;
+import pro.sky.budgetapp.model.Transaction;
 import pro.sky.budgetapp.service.BudgetService;
 
 import java.time.LocalDate;
+import java.time.Month;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 @Service
 public class BudgetServiceImpl implements BudgetService {
 
     public static final int SALARY = 20000;
+    public static final int SAVING = 3000;
+    public static final int DAILY_BUDGET = (SALARY - SAVING) / LocalDate.now().lengthOfMonth(); //бюджет на день
+    public static int balance = 0;
     public static final int AVG_SALARY = (10000 + 10000 + 10000 + 10000 + 10000 + 15000 + 15000 + 15000 + 15000 + 15000 + 15000 + 20000) / 12;
     public static final double AVG_DAYS = 29.3;
+    private static long lastId = 0;
+
+    private static Map<Month, Map<Long, Transaction>> transactions = new TreeMap<>();
 
     @Override
     public int getDailyBudget() {
-        return SALARY / 30;
+        return DAILY_BUDGET;
     }
 
     @Override
-    public int getBalance() {
-        return SALARY - LocalDate.now().getDayOfMonth() * getDailyBudget();
+    public int getBalance() {   //Оствток средств всего (сколько денег кошельке)
+        return SALARY - SAVING - getAllSpend();
+    }
+
+    public void addTransaction(Transaction transaction) { //Добавление транзакции(когда мы покупаем что-то)
+        Map<Long, Transaction> monthTransactions = transactions.getOrDefault(LocalDate.now().getMonth(), new LinkedHashMap<>());
+        monthTransactions.put(lastId++, transaction);
+    }
+
+    public int getDailyBalance() { //Остаток средств на текущий день (сколько еще можно сегодня потратить)
+        return DAILY_BUDGET * LocalDate.now().getDayOfMonth() - getAllSpend();
+    }
+
+    private int getAllSpend() { //Получить все траты (подсчет сколько мы уже потратили)
+        Map<Long, Transaction> monthTransactions = transactions.getOrDefault(LocalDate.now().getMonth(), new LinkedHashMap<>());
+
+
+        int sum = 0;
+        for (Transaction transaction : monthTransactions.values()) {
+            sum += transaction.getSum();
+        } return sum;
     }
 
     @Override
